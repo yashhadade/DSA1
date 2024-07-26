fd12559eace5f945b8c8601ab936f754113d7282
Yash <77961742+yashhadade@users.noreply.github.com>
2024-07-26 12:01:18 +0530

    Create starpattern3.java

---
+++ starpattern3.java
@@ -0,0 +1,14 @@
+package hello.java;
+
+public class starpattern2 {
+	public static void main(String args[]) {
+		int n=6;
+		for (int i=1;i<=n;i++) {
+			for (int j=n;j>=i;j--) {
+			System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+	}
+}

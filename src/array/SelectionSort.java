7c7f25172328113d819e6e64fd2e676e4a60f48d
Yash Hadade <yashhadade13@gmail.com>
2024-11-04 17:16:42 +0530

    merge sort

--- src/array/SelectionSort.java
+++ src/array/SelectionSort.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 public class SelectionSort{
     public static void main (String args[]){
         int a[]={13,46,24,52,20,9};
-        int b=0;
+       
        for (int i=0;i<a.length-1;i++){
         int min =i;
         for(int j=i+1;j<a.length;j++){
